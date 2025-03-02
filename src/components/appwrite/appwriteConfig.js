dff7a060eb24a718457095c4f968e4400de9bdc6
Riktam Nandi <riktamrdxnandi@gmail.com>
2025-03-02 12:25:28 +0530

    minor changes

--- src/components/appwrite/appwriteConfig.js
+++ src/components/appwrite/appwriteConfig.js
@@ -3,8 +3,8 @@ import { Account, Client } from "appwrite";
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(String(import.meta.env.VITE_APPWRITE_PROJECT_ID));
 
 export const account = new Account(client);
 
