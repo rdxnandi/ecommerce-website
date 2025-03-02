0ed48b91abd8a533e6b3cf04bb6c9bdc4013a3d3
Riktam Nandi <riktamrdxnandi@gmail.com>
2025-03-02 12:43:47 +0530

    minor changes

--- src/components/appwrite/appwriteConfig.js
+++ src/components/appwrite/appwriteConfig.js
@@ -3,6 +3,9 @@ import { Client, Account } from "appwrite";
 const endpoint = import.meta.env.VITE_APPWRITE_URL;
 const projectId = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
+console.log("Appwrite Endpoint:", endpoint);
+console.log("Appwrite Project ID:", projectId);
+
 if (!endpoint || !projectId) {
   throw new Error(
     "Appwrite endpoint or project ID is not defined in environment variables"
